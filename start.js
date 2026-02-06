7659d40f6ffe962cdf1e0854791a3fdb6aea751e
widescopeindustries <info@widescopeindustries.com>
2026-02-05 21:26:13 -0600

    fix: Simplify start command, remove wrapper script

--- start.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-const port = process.env.PORT || 3000;
-console.log(`Starting Next.js on port ${port}`);
-
-execSync(`npx next start -H 0.0.0.0 -p ${port}`, {
-  stdio: 'inherit',
-  env: { ...process.env }
-});
